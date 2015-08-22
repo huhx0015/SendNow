75e3b0c5514b0b899b2e221f4efba0c7ba70897e
Michael Y. Huh <mhuh@yoonhuh.com>
2015-08-22 13:18:36 -0700

    * Test commit.

--- mobile/src/main/java/com/vetcon/sendnow/SNMainActivity.java
+++ mobile/src/main/java/com/vetcon/sendnow/SNMainActivity.java
@@ -7,6 +7,7 @@ import android.view.MenuItem;
 
 public class SNMainActivity extends AppCompatActivity {
 
+    //Test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
