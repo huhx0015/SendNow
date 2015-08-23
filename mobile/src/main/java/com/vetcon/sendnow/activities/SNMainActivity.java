7be5dccfdb5cebd2467700c9c4a5aad4d52ace84
farnyu <farn_yu@yahoo.com>
2015-08-23 11:19:09 -0700

    Missing import statement

--- mobile/src/main/java/com/vetcon/sendnow/activities/SNMainActivity.java
+++ mobile/src/main/java/com/vetcon/sendnow/activities/SNMainActivity.java
@@ -23,6 +23,7 @@ import android.widget.ArrayAdapter;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.Toast;
 import com.parse.LogInCallback;
 import com.parse.ParseAnalytics;
